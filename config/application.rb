1f37558eab5101ecf16a14c51a858ce1afb6273f
Thu99 <nguyenvanthu591999@gmail.com>
2020-08-27 16:05:54 +0700

    Init Project

---
+++ config/application.rb
@@ -0,0 +1,13 @@
+require_relative 'boot'
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module SampleApp
+  class Application < Rails::Application
+    config.load_defaults 6.0
+    I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :en
+  end
+end
